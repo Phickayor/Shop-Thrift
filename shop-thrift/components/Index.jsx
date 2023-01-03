ee3b7105587312cf87e2ad5e3538cff74fb8a3e7
Fikayo Jetawo <jetawof@gmail.com>
2023-01-03 03:58:28 +0100

    Navbar component

--- shop-thrift/components/Index.jsx
+++ shop-thrift/components/Index.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
 import Avatar from '../images/bgAvatar.png'
-import Logo from '../images/shop_thrift_logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 function Index() {
     const rightArrow = <FontAwesomeIcon icon={faAngleRight} />
     return (
-        <div className='bg-purple-600 text-white flex h-[85vh]'>
-            <img src={Logo} alt="Shop Thrift" className='fixed m-10 text-3xl font-semibold font-serif text-slate-100' />
-            <div className='w-3/5 flex flex-col justify-center'>
-                <div className='mx-auto w-4/5'>
-                    <h1 className="text-6xl leading-[72px] font-mono font-semibold inline">SHOP FOR THE BEST QUALITY AT IT'S LOWEST PRICES</h1>
-                    <button className='inline-block ml-5 hover:bg-inherit bg-white hover:text-white text-purple-600 text-5xl w-20 h-20 rounded-full border-2 p-4 '>{rightArrow}</button>
+        <>
+            <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white flex'>
+                <div className='w-3/5 flex flex-col justify-center'>
+                    <div className='mx-auto w-4/5'>
+                        <h1 className="text-6xl leading-[72px] font-mono font-semibold inline">SHOP FOR THE BEST QUALITY AT IT'S LOWEST PRICES</h1>
+                        <button className='inline-block ml-5 hover:bg-inherit bg-white hover:text-white text-purple-600 text-5xl w-20 h-20 rounded-full border-2 p-4 '>{rightArrow}</button>
+                    </div>
+                </div>
+                <div className='w-2/5 flex justify-center'>
+                    <Image src={Avatar} className='object-cover h-full z-10 opacity-90' />
                 </div>
             </div>
-            <div className='w-2/5 flex justify-center'>
-                <Image src={Avatar} className='object-cover h-full opacity-90' />
-            </div>
-        </div>
+        </>
     )
 }
 
--- shop-thrift/pages/index.js
+++ shop-thrift/pages/index.js
@@ -1,13 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import React from 'react'
 import Index from '../components/Index'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <Index />
+    <>
+      <Navbar />
+      <Index />
+    </>
   )
 }
